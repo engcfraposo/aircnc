4f57399bd2b2326bbb2bb84376b27c0f4cbc0441
engcfraposo <engcfraposo@gmail.com>
2020-04-08 11:43:19 -0300

    update 08/04/2020
    Semana Omnistack9

---
+++ backend/src/Controllers/SessionController.js
@@ -0,0 +1,18 @@
+const User = require('../Models/User')
+
+
+module.exports = {
+
+    async store (req,res){
+        const { email } = req.body;
+
+        let user = await User.findOne({ email });
+
+        if (!user){
+        const user = await  User.create({email});
+        }
+        
+        return res.json(user)
+    }
+
+};
